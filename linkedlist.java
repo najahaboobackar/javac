b93f34e103f72ba1169682522d1b9d24ea8ab81d
najah <najahaboobacker79@gmail.com>
2024-02-17 23:44:59 +0530

    @

--- linkedlist.java
+++ linkedlist.java
@@ -1,64 +1,85 @@
 public class linkedlist {
-    class Node{
+    class Node {
         int data;
         Node next;
-        Node(int data){
-            this.data=data;
+        Node(int data) {
+            this.data = data;
         }
     }
-    public  Node head=null;
-    public Node tail=null;
-    public void addNode(int data){
-        Node newNode =new Node(data);
-        if(head==null){
-            head=newNode;
-        }
-        else{
-            tail.next=newNode;
-            
-        }
-        tail=newNode;
-        
 
+    public Node head = null;
+    public Node tail = null;
 
+    
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
     }
-    public  void display(){
-        Node temp=head;
-        if(head==null){
+
+    public void display() {
+        Node temp = head;
+        if (head == null) {
             System.out.println("empty");
-            return ;
-
+            return;
         }
-        while(temp!=null){
+        while (temp != null) {
             System.out.println(temp.data);
-            temp=temp.next;
-            
+            temp = temp.next;
         }
-
     }
-    public void delete(int data){
-        Node temp=head;
-        Node prev=null;
-        if(temp.next==null && temp.data==data){
-            System.out.println(temp.data);
-            temp=null;
+
+    public void delete(int data) {
+        Node temp = head;
+        Node prev = null;
+        if (temp != null && temp.data == data) {
+            System.out.println("the deleted data " + temp.data);
+            head = temp.next;
+            return;
+        }
+        while (temp != null && temp.data != data) {
+            prev = temp;
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("No such element found.");
+            return;
+        }
+        prev.next = temp.next;
+        System.out.println("the deleted data " + temp.data);
+    }
+
+    public void insertData(int nexto, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        while(temp.data!=nexto &&temp!=null){
+            temp=temp.next;
+        }
+        if(temp==null){
+            return;
+        }
+        if(temp==tail){
+            tail.next=newNode;
+            tail=newNode;
+            return ;
         }
         else{
-            System.out.println("no same elements");
+        
+            newNode.next=temp.next;
+            temp.next=newNode;
         }
-        while(temp.next!=null ){
-            prev=temp;
-            temp=temp.next;
-            if(temp.data==data){
-                int y=temp.data;
-                prev.next=temp.next;
-                System.out.println("the deleted data "+y);
-            }
+            
             
         }
-    }
-    public static void main(String [] argc){
-        linkedlist li=new linkedlist();
+        
+    
+
+    public static void main(String[] argc) {
+        linkedlist li = new linkedlist();
         li.display();
         li.addNode(1);
         li.addNode(2);
@@ -68,18 +89,10 @@ public class linkedlist {
         li.delete(3);
         li.display();
         System.out.println("after adding 5");
-        li.addNode(5);
+        
         li.display();
-        linkedlist  di=new linkedlist();
-        System.out.println("new object di");
-        di.addNode(1);
-        di.addNode(2);
-        di.display();
-        di.delete(1);
-        di.delete(1);
-
-
-
+        li.insertData(1,5);
+        System.out.println("after addding 5");
+        li.display();
     }
-    
 }

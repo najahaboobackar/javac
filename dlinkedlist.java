e8f55bc0625b459affb3866f4b85ffab2a65d6ec
najah <najahaboobacker79@gmail.com>
2024-02-19 23:11:43 +0530

    @

---
+++ dlinkedlist.java
@@ -0,0 +1,42 @@
+public class dlinkedlist {
+    class Node{
+        int data;
+        Node next;
+        Node prev;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public Node head;
+    public Node tail;
+    public void addNode(int data){
+        Node newnode=new Node(data);
+        if(head==null){
+            head=newnode;
+        }
+        else{
+            newnode.next=head.next;
+            head.next=newnode;
+            newnode.prev=head;
+        }
+        tail=newnode;
+    
+    }
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        dlinkedlist d=new dlinkedlist();
+        d.addNode(2);
+        d.addNode(3);
+        d.addNode(4);
+        d.display();
+        
+    }
+
+    
+}
